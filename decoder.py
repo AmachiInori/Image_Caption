5c438c6cd961fc834b0bac20f7388ea440ce4b39
天知いのり <65227082+AmachiInori@users.noreply.github.com>
2022-01-08 01:59:04 +0800

    first commit

---
+++ decoder.py
@@ -0,0 +1,186 @@
+import numpy as np
+from numpy import array
+import os
+from PIL import Image
+import pickle
+import string
+from keras.optimizers import Adam, RMSprop
+from keras.models import Sequential
+from keras.layers.wrappers import Bidirectional
+from keras.preprocessing import sequence
+from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector, Activation, concatenate, Flatten, Merge
+
+import pandas as pd
+import glob
+
+
+captionFile = 'Flickr8k_text/Flickr8k.token.txt'
+captions = open(captionFile, 'r').read().strip().split('\n')
+
+imagesPath = 'Flickr8k_Dataset/Flicker8k_Dataset/'
+images = glob.glob(imagesPath+'*.jpg')
+
+
+def split_data(dict):
+    temp = []
+    for i in images:
+        if i[len(imagesPath):] in dict:
+            temp.append(i)
+    return temp
+
+
+trainImagesIndexFile = 'Flickr8k_text/Flickr_8k.trainImages.txt'
+trainImagesIndex = set(open(trainImagesIndexFile, 'r').read().strip().split('\n'))
+trainImages = split_data(trainImagesIndex)
+
+validationImagesIndexFile = 'Flickr8k_text/Flickr_8k.devImages.txt'
+validationImagesIndex = set(open(validationImagesIndexFile, 'r').read().strip().split('\n'))
+validationImages = split_data(validationImagesIndex)
+
+testImagesIndexFile = 'Flickr8k_text/Flickr_8k.testImages.txt'
+testImagesIndex = set(open(testImagesIndexFile, 'r').read().strip().split('\n'))
+testImages = split_data(testImagesIndex)
+
+
+encoding_train = pickle.load(open('encoded_tra.p', 'rb'))
+encoding_test = pickle.load(open('encoded_tes.p', 'rb'))
+
+dictionary = {}
+for i, caption in enumerate(captions):
+    caption = caption.split('\t')
+    caption[0] = caption[0][:len(caption[0])-2]
+    if caption[0] in dictionary:
+        dictionary[caption[0]].append(caption[1])
+    else:
+        dictionary[caption[0]] = [caption[1]]
+
+
+trainDictionary = {}
+for i in trainImages:
+    if i[len(imagesPath):] in dictionary:
+        trainDictionary[i] = dictionary[i[len(imagesPath):]]
+
+validationDictionary = {}
+for i in validationImages:
+    if i[len(imagesPath):] in dictionary:
+        validationDictionary[i] = dictionary[i[len(imagesPath):]]
+
+testDictionary = {}
+for i in testImages:
+    if i[len(imagesPath):] in dictionary:
+        testDictionary[i] = dictionary[i[len(imagesPath):]]
+
+
+Sent = []
+for key, val in trainDictionary.items():
+    for i in val:
+        Sent.append('<start> ' + i + ' <end>')
+words = [i.split() for i in Sent]
+unique = []
+for i in words:
+    unique.extend(i)
+unique = list(set(unique))
+
+word2idx = {val: index for index, val in enumerate(unique)}
+idx2word = {index: val for index, val in enumerate(unique)}
+
+maxSenLen = 0
+for s in Sent:
+    s = s.split()
+    if len(s) > maxSenLen:
+        maxSenLen = len(s)
+
+
+f = open('training_captions.txt', 'w')
+
+f.write("image_id\tcaptions\n")
+for key, val in trainDictionary.items():
+    for i in val:
+        f.write(key[len(imagesPath):] + "\t" + "<start> " + i +" <end>" + "\n")
+
+f.close()
+
+df = pd.read_csv('training_captions.txt', delimiter='\t')
+c = [i for i in df['captions']]
+imgs = [i for i in df['image_id']]
+
+vocab_size = len(unique)
+samples_per_epoch = 0
+for s in Sent:
+    samples_per_epoch += len(s.split())-1
+
+
+def data_generator(batch_size=32):
+    partial_caps = []
+    next_words = []
+    images = []
+
+    df = pd.read_csv('training_captions.txt', delimiter='\t')
+    df = df.sample(frac=1)
+    iter = df.iterrows()
+    c = []
+    imgs = []
+    for i in range(df.shape[0]):
+        x = next(iter)
+        c.append(x[1][1])
+        imgs.append(x[1][0])
+
+    count = 0
+    while True:
+        for j, text in enumerate(c):
+            current_image = encoding_train[imgs[j]]
+            for i in range(len(text.split()) - 1):
+                count += 1
+
+                partial = [word2idx[txt] for txt in text.split()[:i + 1]]
+                partial_caps.append(partial)
+
+                # Initializing with zeros to create a one-hot encoding matrix
+                # This is what we have to predict
+                # Hence initializing it with vocab_size length
+                n = np.zeros(vocab_size)
+                # Setting the next word to 1 in the one-hot encoded matrix
+                n[word2idx[text.split()[i + 1]]] = 1
+                next_words.append(n)
+
+                images.append(current_image)
+
+                if count >= batch_size:
+                    next_words = np.asarray(next_words)
+                    images = np.asarray(images)
+                    partial_caps = sequence.pad_sequences(partial_caps, maxlen=maxSenLen, padding='post')
+                    yield [[images, partial_caps], next_words]
+                    partial_caps = []
+                    next_words = []
+                    images = []
+                    count = 0
+
+
+embedding_size = 300
+
+image_model = Sequential([
+        Dense(embedding_size, input_shape=(2048,), activation='relu'),
+        RepeatVector(maxSenLen)
+    ])
+
+caption_model = Sequential([
+        Embedding(vocab_size, embedding_size, input_length=maxSenLen),
+        LSTM(256, return_sequences=True),
+        TimeDistributed(Dense(300))
+    ])
+
+final_model = Sequential([
+        Merge([image_model, caption_model], mode='concat', concat_axis=1),
+        Bidirectional(LSTM(256, return_sequences=False)),
+        Dense(vocab_size),
+        Activation('softmax')
+    ])
+
+final_model.compile(loss='categorical_crossentropy', optimizer=RMSprop(), metrics=['accuracy'])
+
+final_model.summary()
+
+final_model.fit_generator(data_generator(batch_size=128), samples_per_epoch=samples_per_epoch, nb_epoch=15,
+                          verbose=2)
+
+final_model.save_weights('LSTM_loss.h5')
---
+++ encoder.py
@@ -0,0 +1,83 @@
+import glob
+import numpy as np
+from keras.models import Model
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from tqdm import tqdm
+import pickle
+
+imagesPath = 'Flickr8k_Dataset/Flicker8k_Dataset/'
+images = glob.glob(imagesPath+'*.jpg')
+
+# 处理数据集
+
+
+def preprocess_input(x):
+    x /= 255.
+    x -= 0.5
+    x *= 2.
+    return x
+
+
+def split_data(dict):
+    temp = []
+    for i in images:
+        if i[len(imagesPath):] in dict:
+            temp.append(i)
+    return temp
+
+
+def preprocess_one(imageName):
+    img = image.load_img(imageName, target_size=(299, 299))
+    res = image.img_to_array(img)
+    res = np.expand_dims(res, axis=0)
+    res = preprocess_input(res)
+    return res
+
+
+trainImagesIndexFile = 'Flickr8k_text/Flickr_8k.trainImages.txt'
+trainImagesIndex = set(open(trainImagesIndexFile, 'r').read().strip().split('\n'))
+trainImages = split_data(trainImagesIndex)
+
+validationImagesIndexFile = 'Flickr8k_text/Flickr_8k.devImages.txt'
+validationImagesIndex = set(open(validationImagesIndexFile, 'r').read().strip().split('\n'))
+validationImages = split_data(validationImagesIndex)
+
+testImagesIndexFile = 'Flickr8k_text/Flickr_8k.testImages.txt'
+testImagesIndex = set(open(testImagesIndexFile, 'r').read().strip().split('\n'))
+testImages = split_data(testImagesIndex)
+
+# 编码器构造
+model = InceptionV3(weights='imagenet')
+
+myInput = model.input
+hidden_layer = model.layers[-2].output
+myModel = Model(myInput, hidden_layer)
+
+
+def encode(image):
+    image = preprocess_one(image)
+    res = myModel.predict(image)
+    res = np.reshape(res, res.shape[1])
+    return res
+
+
+def encode_total():
+    encodingTrain = {}
+    for img in tqdm(trainImages):
+        encodingTrain[img[len(imagesPath):]] = encode(img)
+
+    with open("encoded_tra.p", "wb") as encodedPickle:
+        pickle.dump(encodingTrain, encodedPickle)
+
+    encodingTest = {}
+    for img in tqdm(testImages):
+        encodingTest[img[len(imagesPath):]] = encode(img)
+
+    with open("encoded_tes.p", "wb") as encodedPickle:
+        pickle.dump(encodingTest, encodedPickle)
+
+    return encodingTrain, encodingTest
+
+
+encode_total()
